38d09ad8345122bc7dab0a7d3c6a6e63d5797b05
François Lecomte <francois.lecomte-ext9sgcib.com>
2015-05-20 10:00:51 +0200

    under dev

--- src/main/java/com/stringcalculator/StringCalculator.java
+++ src/main/java/com/stringcalculator/StringCalculator.java
@@ -2,4 +2,79 @@ package com.stringcalculator;
 
 public class StringCalculator {
 
+	private static final String SEPARATOR_STOP = "\n";
+	private static final String SEPARATOR_START = "//";
+	private static final String[] SEPARATORS = new String[] { ",", SEPARATOR_STOP };
+
+	public int add(String operation) {
+
+		String[] separators = getSeparators(operation);
+
+		String sumString = getSumString(operation);
+
+		return add(sumString, separators);
+	}
+
+	private String getSumString(String operation) {
+		return separatorSpecified(operation) ? after(operation, SEPARATOR_STOP) : operation;
+	}
+
+	private int add(String sumString, String[] separators) {
+		int separatorIndex = nextSeparatorIndex(sumString, separators);
+		int sum = asInt(before(sumString, separatorIndex));
+
+		if (separatorIndex < sumString.length()) {
+			sum += add(after(sumString, separatorIndex), separators);
+		}
+
+		return sum;
+	}
+
+	private String[] getSeparators(String operation) {
+		if (separatorSpecified(operation)) {
+			return new String[] { between(operation, SEPARATOR_START, SEPARATOR_STOP) };
+		} else {
+			return SEPARATORS;
+		}
+	}
+
+	private boolean separatorSpecified(String string) {
+		return string.startsWith(SEPARATOR_START);
+	}
+
+	private String after(String string, String separator) {
+		return after(string, string.indexOf(separator));
+	}
+
+	private String after(String string, int index) {
+		return string.substring(index + 1);
+	}
+
+	private String before(String string, int index) {
+		return string.substring(0, index);
+	}
+
+	private String between(String string, String start, String stop) {
+		return string.substring(string.indexOf(start) + start.length(), string.indexOf(stop));
+	}
+
+	private boolean separatorFound(int separatorIndex) {
+		return separatorIndex != -1;
+	}
+
+	private int nextSeparatorIndex(String sumString, String[] separators) {
+		int separatorIndex = sumString.length();
+		for (String separator : separators) {
+			int nextSeparatorIndex = sumString.indexOf(separator);
+			if (separatorFound(nextSeparatorIndex)) {
+				separatorIndex = Math.min(separatorIndex, nextSeparatorIndex);
+			}
+		}
+		return separatorIndex;
+	}
+
+	private static int asInt(String number) {
+		return number.isEmpty() ? 0 : Integer.parseInt(number);
+	}
+
 }
--- src/test/java/com/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/stringcalculator/StringCalculatorTest.java
@@ -1,4 +1,46 @@
 package com.stringcalculator;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringCalculatorTest {
+
+	private StringCalculator calculator;
+
+	@Before
+	public void setup() {
+		calculator = new StringCalculator();
+	}
+
+	@Test
+	public void should_return_0_when_empty_string_sent() {
+		assertEquals(0, calculator.add(""));
+	}
+
+	@Test
+	public void should_return_input_as_an_integer_when_single_number_sent() {
+		assertEquals(9, calculator.add("9"));
+	}
+
+	@Test
+	public void should_return_sum_when_two_numbers_sent() {
+		assertEquals(14, calculator.add("9,5"));
+	}
+
+	@Test
+	public void should_return_sum_when_more_than_two_numbers_sent() {
+		assertEquals(37, calculator.add("9,5,8,9,6"));
+	}
+
+	@Test
+	public void should_return_sum_when_new_line_separator() {
+		assertEquals(6, calculator.add("1\n2,3"));
+	}
+
+	@Test
+	public void should_return_sum_when_defined_separator() {
+		assertEquals(6, calculator.add("//;\n1;2;3"));
+	}
 }
