e9a2d4370a1ee2d43223d157efaec0ba9da85636
François Lecomte <francois.lecomte-ext9sgcib.com>
2015-05-26 17:18:22 +0200

    remove static

--- src/main/java/com/stringcalculator/AdditionBuilder.java
+++ src/main/java/com/stringcalculator/AdditionBuilder.java
@@ -13,28 +13,37 @@ public class AdditionBuilder {
 	private static final String SEPARATOR_STOP = "\n";
 	private static final String SEPARATOR_START = "//";
 	private static final String[] SEPARATORS = new String[] { ",", SEPARATOR_STOP };
+	private final String additionString;
+
+	public AdditionBuilder(String additionString) {
+		this.additionString = additionString;
+	}
 
 	public static Addition buildAddition(String additionString) {
+		return new AdditionBuilder(additionString).buildAddition();
+	}
+
+	private Addition buildAddition() {
 		String[] separators = extractSeparators(additionString);
 		String sumString = extractSumString(additionString);
 		List<Integer> numbers = extractNumbers(sumString, separators);
 		return new Addition(numbers);
 	}
 
-	private static String extractSumString(String additionString) {
+	private String extractSumString(String additionString) {
 		return separatorSpecified(additionString) ? after(additionString, SEPARATOR_STOP) : additionString;
 	}
 
-	private static boolean separatorSpecified(String string) {
+	private boolean separatorSpecified(String string) {
 		return string.startsWith(SEPARATOR_START);
 	}
 
-	private static String[] extractSeparators(String operation) {
+	private String[] extractSeparators(String operation) {
 		return separatorSpecified(operation) ? new String[] { between(operation, SEPARATOR_START, SEPARATOR_STOP) }
 				: SEPARATORS;
 	}
 
-	private static List<Integer> extractNumbers(String sumString, String[] separators) {
+	private List<Integer> extractNumbers(String sumString, String[] separators) {
 		int separatorIndex = nextSeparatorIndex(sumString, separators);
 		int number = asInt(before(sumString, separatorIndex));
 
@@ -47,11 +56,11 @@ public class AdditionBuilder {
 		return numbers;
 	}
 
-	private static boolean separatorFound(int separatorIndex) {
+	private boolean separatorFound(int separatorIndex) {
 		return separatorIndex != -1;
 	}
 
-	private static int nextSeparatorIndex(String sumString, String[] separators) {
+	private int nextSeparatorIndex(String sumString, String[] separators) {
 		int separatorIndex = sumString.length();
 		for (String separator : separators) {
 			int nextSeparatorIndex = sumString.indexOf(separator);
